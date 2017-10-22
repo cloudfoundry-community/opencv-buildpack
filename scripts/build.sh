79aecfcecc48ab2fa3fa2790cb0b815ca55374c9
Dr Nic Williams <drnicwilliams@gmail.com>
2017-10-23 09:52:52 +1000

    noop for bin/supply

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -ex
+
+ROOTDIR="$( dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" )"
+BINDIR=$ROOTDIR/bin
+
+export GOPATH=$ROOTDIR
+export GOOS=linux
+
+go build -ldflags="-s -w" -o $BINDIR/supply redis/supply/cli
