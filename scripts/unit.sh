57ff995049ff37e349324b40e8f9d057a9bc2bc6
Dr Nic Williams <drnicwilliams@gmail.com>
2017-10-24 07:07:11 +1000

    unit.sh self-sources .envrc

--- scripts/unit.sh
+++ scripts/unit.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 buildpack=opencv
 
 export ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+cd $ROOT
+source .envrc
 
 if [ ! -f $ROOT/.bin/ginkgo ]; then
   (cd $ROOT/src/$buildpack/vendor/github.com/onsi/ginkgo/ginkgo/ && go install)
