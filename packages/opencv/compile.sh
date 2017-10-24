bda59998486c6599ef943af5931c3a2a0390771f
Dr Nic Williams <drnicwilliams@gmail.com>
2017-10-24 16:30:02 +1000

    /tmp/build happens to be concourse working dir

--- packages/opencv/compile.sh
+++ packages/opencv/compile.sh
@@ -7,14 +7,15 @@ VERSION=${VERSION:-$(cat ${VERSION_FROM})}
 
 : ${SRC_DIR:?required}
 : ${OUTPUT_DIR:?required}
-TMP_SRC_DIR=${TMP_DIR:-/tmp/src}
-TMP_BUILD_DIR=${TMP_DIR:-/tmp/build}
+TMP_SRC_DIR=${TMP_DIR:-tmp/src}
+TMP_BUILD_DIR=${TMP_DIR:-tmp/build}
 
 SRC_ZIP=$PWD/$(ls $SRC_DIR/*.zip)
 
-ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
-SRC_DIR=${ROOTDIR}/${SRC_DIR}
-OUTPUT_DIR=${ROOTDIR}/${OUTPUT_DIR}
+SRC_DIR=$PWD/${SRC_DIR}
+OUTPUT_DIR=$PWD/${OUTPUT_DIR}
+TMP_SRC_DIR=$PWD/${TMP_SRC_DIR}
+TMP_BUILD_DIR=$PWD/${TMP_BUILD_DIR}
 
 if [[ "${SKIP_INSTALL_LINUX_PKG:-X}" == "X" ]]; then
   apt-get update
@@ -41,11 +42,9 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=${TMP_BUILD_DIR} ..
 make
 make install
 
-cd ${ROOTDIR}
-
-mkdir -p ${OUTPUT_DIR}/blobs
-mkdir -p ${OUTPUT_DIR}/manifest
+mkdir -p $OUTPUT_DIR/blobs
+mkdir -p $OUTPUT_DIR/manifest
 
 cd $TMP_BUILD_DIR
-tar cfz ${OUTPUT_DIR}/opencv-compiled-${VERSION}.tgz .
+tar cfz $OUTPUT_DIR/opencv-compiled-${VERSION}.tgz .
 cd -
