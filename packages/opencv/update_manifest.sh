d40a90c4658f1e766af0361754b5d247dbfe7a2e
Dr Nic Williams <drnicwilliams@gmail.com>
2017-10-26 06:33:41 +1000

    until pushback from users, bump default to latest

--- packages/opencv/update_manifest.sh
+++ packages/opencv/update_manifest.sh
@@ -18,10 +18,13 @@ TMP_DIR=${TMP_DIR:-/tmp}
 BLOB=$(ls $BLOB)
 
 cat > $TMP_DIR/dependencies.yml <<YAML
+default_versions:
+- name: ${BLOB_NAME}
+  version: ${BLOB_NAME}
 dependencies:
 - ((merge on version))
 - name:    ${BLOB_NAME}
-  version: ${VERSION}
+  version: ${BLOB_NAME}
   uri:     "${DOWNLOAD_ROOT_URL}/blobs/${BLOB_NAME}/$(basename ${BLOB})"
   md5:     "$(md5sum ${BLOB} | awk '{print $1}')"
   cf_stacks: [cflinuxfs2]
@@ -41,7 +44,7 @@ fi
 cat <<EOF >>${REPO_OUT}/ci/release_notes.md
 
 ## ${BLOB_NAME}
-Bumped to v${VERSION}
+Bumped to v${VERSION}. Made the default.
 EOF
 
 # GIT!
